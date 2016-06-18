fb47671071e5c74142c00b5857aba0c8104c052c
Daniel Klein <othercriteria@gmail.com>
2016-06-18 12:48:54 -0400

    Various changes to enable hindent in haskell-mode

--- init.el
+++ init.el
@@ -1,13 +1,11 @@
 (require 'package)
+(package-initialize)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ '(hindent-style "gibiansky")
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
+     ("melpa" . "http://melpa.org/packages/")))))
 
 ;; http://emacsredux.com/blog/2013/05/31/highlight-lines-that-exceed-a-certain-length-limit/
 (require 'whitespace)
@@ -19,3 +17,4 @@
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(add-hook 'haskell-mode-hook #'hindent-mode)
